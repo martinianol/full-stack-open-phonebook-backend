323979e8e8a53359d12c6d068d388e513df22f80
Mars <martinianol@outlook.com>
2021-12-13 15:59:23 -0300

    3.11 phonebook full stack

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ morgan.token('body', (request, response) => JSON.stringify(request.body));
 app.use(morgan(':method :url :status :req[content-length] - :response-time ms :body'));
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 let phonebook = [
   {
