ce5a1cd93f16a1934dd7083296a3008f95d7de09
Mars <martinianol@outlook.com>
2021-12-14 15:09:35 -0300

    3.16: Phonebook database, step4

--- index.js
+++ index.js
@@ -114,6 +114,23 @@ app.post('/api/persons/', (request, response) => {
 
 })
 
+/**
+ * Error Handler
+ */
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT
 app.listen(PORT)
