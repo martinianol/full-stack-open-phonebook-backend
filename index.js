aefb112905de98ffa309586139636496121843fe
Mars <martinianol@outlook.com>
2021-12-15 22:43:53 -0300

    Remove Hard coded data

--- index.js
+++ index.js
@@ -15,29 +15,6 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
 
-let phonebook = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
-
 app.get('/api/persons', (request, response, next) => {
   Person.find({})
     .then(persons => {
